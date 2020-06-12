f4d65a45a1ca3336e92d87d975d9432663a1b52c
deltaloose <takeshiroyu@gmail.com>
2020-06-12 23:31:06 +0900

    add-sweets_new

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,7 +1,12 @@
 class SweetsController < ApplicationController
+  #おかし新規登録ページの表示
   def new
+    @sweet = Sweet.new
   end
   def create
+    @sweet = Sweet.new(sweet_params)
+    @sweet.save
+    redirect_to sweets_path
   end
   def index
   end
