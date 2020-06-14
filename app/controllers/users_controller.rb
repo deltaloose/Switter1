b072116b29565579e471c20f0296e4a5c7c45c0e
deltaloose <takeshiroyu@gmail.com>
2020-06-14 16:05:18 +0900

    add-user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
   def show
+    @user = User.find(params[:id])
+    @sweets = @user.sweets
   end
   def edit
+    if @user = current_user
+    else
+       render :show
+    end
+  end
   end
   def update
+    @user = current_user
+    if @user.update(user_params)
+       redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
